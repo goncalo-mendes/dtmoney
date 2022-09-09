f743bb8b5bf8e27eee57f516c35691063a93f126
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2022-09-09 11:50:11 +0100

    chore: add styled components,create global styles and import fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyled } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1> Hello word </h1>
+      <GlobalStyled />
     </div>
   );
 }
