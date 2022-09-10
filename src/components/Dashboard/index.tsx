375325a70854e0d43af1f6b09083fb7591eb69ae
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2022-09-10 11:41:53 +0100

    chore: create transactionTable component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable/indes";
 import { Container } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   )
 }
