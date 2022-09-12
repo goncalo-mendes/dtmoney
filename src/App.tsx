0439fa2bd7650dc648e983c1a508bb3adaaf4a04
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2022-09-12 13:50:51 +0100

    chore: create modal form for transactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyled } from "./styles/global";
+import Modal from 'react-modal';
+import { useState } from "react";
+import { NewTransactionsModal } from "./components/NewTransactionsModal";
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionsModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyled />
     </>
   );
